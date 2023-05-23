275d5b573bc0d9ccc2060c0e82eef180f9d6261b
pyrosec <inquiries@pyrosec.gg>
2023-05-23 19:37:23 +0000

    update

--- lib.commonjs/cli.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./logger.js";
-export declare function runCLI(): Promise<void>;
-//# sourceMappingURL=cli.d.ts.map
--- lib.commonjs/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./proxies-fo.js";
-export * from "./logger.js";
-//# sourceMappingURL=index.d.ts.map
